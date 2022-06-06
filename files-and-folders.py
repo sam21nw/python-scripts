9ee131c95ffc2fca89947069eb6b946c30cb1342
Sam Jacob <sam21nw@gmail.com>
2022-06-06 18:49:43 +0400

    updated files-and-folders

--- files-and-folders.py
+++ files-and-folders.py
@@ -3,26 +3,29 @@ from datetime import datetime
 from pathlib import Path
 import os
 
-folder = input("Enter folder: ")
-our_folder = Path(folder)
+while True:
+    folder = input("Input a Folder: ")
+    if Path(folder).is_dir() == False:
+        print(f"Input a valid Folder")
+    if Path(folder).is_dir() == True and not "".__eq__(folder):
+        print(f"Input folder is '{folder}'")
+        our_folder = Path(folder)
+        break
 
-if not our_folder.is_dir():
-    print(f"{our_folder} is not a folder..")
-else:
-    print(f"{our_folder} is selected")
 
 # %%
 if not our_folder.is_dir():
     print("Not a folder..")
 else:
+    replace_text = input("Replace text: ")
+    with_text = input("With text: ")
+
     for file in our_folder.iterdir():
 
         dir = file.parent
 
         f = Path(file)
         old_name, ext = f.stem, f.suffix
-        new_name = f"{old_name.replace('sample', 'test')}{ext}"
+        new_name = f"{old_name.replace(replace_text, with_text)}{ext}"
 
         file.rename(Path(dir, new_name))
-
-# %%
